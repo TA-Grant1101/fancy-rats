903ebda49c5c820a701811fc2ad299fcd3a749d6
Jonathan <byersj920@gmail.com>
2024-03-18 19:37:46 -0500

    Search controller now properly connects database to search.js

--- backend/src/main/java/org/launchcode/fancyrats/controllers/SearchController.java
+++ backend/src/main/java/org/launchcode/fancyrats/controllers/SearchController.java
@@ -3,19 +3,32 @@ package org.launchcode.fancyrats.controllers;
 import org.launchcode.fancyrats.models.Job;
 import org.launchcode.fancyrats.models.data.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/search")
 public class SearchController {
 
     @Autowired
     private JobRepository jobRepository;
 
-    @RequestMapping("/search")
-    public List<Job> getJobList(){
-        return jobRepository.findAll();
+    @GetMapping
+    public List<Job> getJobList(@RequestParam(required = false) Integer id){
+        List<Job> returnedJobList = new ArrayList<>();
+
+        if (id == null){return jobRepository.findAll();}
+        else{
+            Optional<Job> optionalJob= jobRepository.findById(id);
+            if (optionalJob.isPresent()){
+                returnedJobList.add(optionalJob.get());
+            } else return jobRepository.findAll();
+        }
+        return returnedJobList;
     }
+
 }
