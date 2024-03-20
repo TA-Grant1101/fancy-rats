54b5d4dcc562421b68dc57d76ec000b246ab4b6b
Jonathan <byersj920@gmail.com>
2024-03-20 15:51:51 -0500

    Searching is now filtering properly.

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -2,18 +2,44 @@ import React, { useState, useEffect } from 'react';
 
 const JobList = () => {
     const [jobs, setJobList] = useState([]);
+    const [id, setId] = useState('');
 
     useEffect(() => {
-        fetch('/search?id=1')
+        fetch(`/search`)
         .then(response => response.json())
-        .then(data => {setJobList(data);});
-    }, []);
+        .then(data => {setJobList(data);})
+        .catch(error => console.error('Error fetching data:', error));
+    }, [id]);
 
-    console.log(jobs);
+    const findById = (idNumber) =>{
+        fetch(`/search?id=${idNumber}`)
+            .then(response => response.json())
+            .then(data => {setJobList(data);})
+            .catch(error => console.error('Error fetching data:', error));
+    };
+
+    const handleInputChange = (event) => {
+        setId(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (id) {findById(id);}
+    };
 
     return (
         <div>
             <h1>List of Jobs</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Search By Id<br></br></label>
+                <input 
+                    type="text" 
+                    name="id" 
+                    value={id} 
+                    onChange={handleInputChange} 
+                    placeholder="Enter ID"/>
+                <button type="submit">Submit</button>
+            </form>
             <table>
                 <tbody>
                     {jobs.map(job => (
