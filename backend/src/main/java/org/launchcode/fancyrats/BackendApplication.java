13649397cead7165a5e6bc0026fe5b0d69eca11a
Bryan Wulfert <bwulfert@gmail.com>
2024-03-21 18:57:09 -0500

    spring security

--- backend/src/main/java/org/launchcode/fancyrats/BackendApplication.java
+++ backend/src/main/java/org/launchcode/fancyrats/BackendApplication.java
@@ -1,7 +1,11 @@
 package org.launchcode.fancyrats;
 
-import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class BackendApplication {
---
+++ backend/src/main/java/org/launchcode/fancyrats/models/ERole.java
@@ -0,0 +1,7 @@
+package org.launchcode.fancyrats.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
