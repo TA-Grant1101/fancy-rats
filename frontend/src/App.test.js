2334b58a82bf988503db534ee6bd582c22b23185
Bryan Wulfert <bwulfert@gmail.com>
2024-03-22 22:59:07 -0500

    changes to build files

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders clients', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/clients/i);
-  expect(headerElement).toBeInTheDocument();
-});
