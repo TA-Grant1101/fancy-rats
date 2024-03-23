665f2a734515adaf682faada1618ec14189a5994
ArielleWaks <ariellemwaks@gmail.com>
2024-03-23 14:36:54 -0500

    Create job form saving to database

--- frontend/src/components/CreateJob.js
+++ frontend/src/components/CreateJob.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import { Button, TextField }  from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 import EventBus from "../common/EventBus";
+import dayjs from "dayjs";
 
 const API_URL = "http://localhost:3000/api";
 
 export default function CreateJob() {
     const [content, setContent] = useState( '');
     const [formData, setFormData] = useState({
-        username: '',
-        password: '',
+        startDate: dayjs(),
+        endDate: dayjs(),
+        zipCode: '',
+        payRate: '',
+        totalHours: '',
+        description: ''
     });
 
     useEffect( () => {
@@ -79,6 +84,14 @@ export default function CreateJob() {
         setFormData({ ...formData, [name]: value });
     };
 
+    const handleStartDateChange = (e) => {
+        setFormData({ ...formData, startDate: e });
+    };
+
+    const handleEndDateChange = (e) => {
+        setFormData({ ...formData, endDate: e });
+    };
+
     return (
         <div className="container">
             <header className="jumbotron">
@@ -86,12 +99,55 @@ export default function CreateJob() {
             </header>
             <form  onSubmit={handleSubmit}>
                 <TextField
-                    label="Username"
+                    label="Zip Code"
                     variant="outlined"
                     fullWidth
-                    name="username"
-                    value={formData.username}
+                    name="zipCode"
+                    value={formData.zipCode}
                     onChange={handleChange}
+                    required
+                />
+                <TextField
+                    label="Pay Rate"
+                    variant="outlined"
+                    fullWidth
+                    name="payRate"
+                    value={formData.payRate}
+                    onChange={handleChange}
+                    required
+                />
+                <TextField
+                    label="Number of Hours"
+                    variant="outlined"
+                    fullWidth
+                    name="totalHours"
+                    value={formData.totalHours}
+                    onChange={handleChange}
+                    required
+                />
+                <TextField
+                    label="Description"
+                    variant="outlined"
+                    fullWidth
+                    name="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                    required
+                />
+                <DatePicker
+                    name="startDate"
+                    value={formData.startDate}
+                    onChange={handleStartDateChange}
+                    label="Start Date"
+                    disablePast
+                    required
+                />
+                <DatePicker
+                    name="endDate"
+                    value={formData.endDate}
+                    onChange={handleEndDateChange}
+                    label="End Date"
+                    disablePast
                     required
                 />
                 <Button type="submit" variant="contained" color="primary">
